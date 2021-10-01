7e28fd817913b04022c3ef29c23090322426e1f9
cauaMascena <cauasinho656@gmail.com>
2021-10-01 11:29:20 -0300

    commit 01/10/2021

--- categorias/acoes.php
+++ categorias/acoes.php
@@ -1,7 +1,22 @@
 <?php
-
+session_start();
 /*CONEXÃO COM BANCO DE DADOS*/
 require('../database/conexao.php');
+
+/*FUNÇÃO DE VALIDAÇÃO*/
+function validaCampos(){
+
+    $erros = [];
+
+    if(!isset($_POST['descricao']) || $_POST['descricao'] == ""){
+
+        $erros[] = "O campo descrição é de preenchimento obrigatório";
+
+    }
+
+    return $erros;
+
+}
 
 /*
 TRATAMENTO DOS DADOS VINDOS DO FORMULÁRIO
@@ -11,7 +26,22 @@ TRATAMENTO DOS DADOS VINDOS DO FORMULÁRIO
 
 */
 switch ($_POST['acao']) {
+
     case 'inserir':
+
+        //CHAMADA DA FUNÇÃO DE VALIDAÇÃO DE ERROS:
+        $erros = validaCampos();
+
+        //VERIFICAR SE EXISTEM ERROS:
+        if(count($erros) > 0){
+
+            $_SESSION["erros"] = $erros;
+
+            header('location: index.php');
+
+            exit();
+
+        }
 
         // echo 'INSERIR';exit;
 
@@ -38,6 +68,18 @@ switch ($_POST['acao']) {
         // exit;
 
         break;
+
+        case 'deletar':
+
+            $categoriaID = $_POST['categoriaId'];
+
+            $sql = "DELETE FROM tbl_categoria WHERE id = $categoriaID";
+
+            $resultado = mysqli_query($conexao, $sql);
+
+            header('location: index.php');
+
+            break;
     
     default:
         # code...
--- categorias/index.php
+++ categorias/index.php
@@ -1,20 +1,21 @@
 <?php
 
-require('../database/conexao.php');
+    session_start();
 
-$sql = "SELECT * FROM tbl_categoria";
+    require('../database/conexao.php');
 
-$resultado = mysqli_query($conexao, $sql);
+    $sql = "SELECT * FROM tbl_categoria";
 
-// echo '<pre>';
-// var_dump($resultado);
-// echo '</pre>';
-// exit;
+    $resultado = mysqli_query($conexao, $sql);
+
+    // $categoria = mysqli_fetch_array($resultado);
+
+    // echo '<pre>';
+    // var_dump($categoria);
+    // echo '</pre>';
+    // exit;
 
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -37,33 +38,53 @@ $resultado = mysqli_query($conexao, $sql);
                 <form class="form-categoria" method="POST" action="./acoes.php">
                     <input type="hidden" name="acao" value="inserir" />
                     <h1 class="span2">Adicionar Categorias</h1>
+                    
                     <ul>
+                    <?php
+                        if(isset($_SESSION["erros"])){
 
+                            foreach ($_SESSION["erros"] as $erro) {
+                                
+                    ?>
+
+                        <li><?php echo $erro?></li>
+
+                    <?php
+                        }//fim do foreach
+
+                        //Limpa a variável de sessão
+                        session_unset();
+                        
+                        //Destroi a sessão
+                        session_destroy();
+
+                    }//fim do if 
+                    ?>
                     </ul>
+
                     <div class="input-group span2">
                         <label for="descricao">Descrição</label>
-                        <input type="text" name="descricao" id="descricao" />
+                        <input type="text" name="descricao" id="descricao"/>
                     </div>
                     <button type="button" onclick="javascript:window.location.href = '../produtos/'">Cancelar</button>
                     <button>Salvar</button>
                 </form>
                 <h1>Lista de Categorias</h1>
 
-                <?php
+                    <?php
+                    
+                        while($categoria = mysqli_fetch_array($resultado)){
 
-                while($categoria = mysqli_fetch_array($resultado)) {
+                    ?>
 
-                    echo $categoria["descricao"];
+                    <div class="card-categorias">
+                        <?php echo $categoria["descricao"]; ?>
+                        <img onclick="deletar(<?php echo $categoria['id']; ?>)" src="https://icons.veryicon.com/png/o/construction-tools/coca-design/delete-189.png" />
+                        <img onclick="javascript: window.location = 'editar.php?id=<?php echo $categoria['id']; ?>'" src="https://icons.veryicon.com/png/o/object/lucq-backstage/edit-45.png" />
 
-                }
+                    </div>
 
-                exit;
-
-                ?>
-
-                <div class="card-categorias">
-                    <img onclick="deletar()" src="https://icons.veryicon.com/png/o/construction-tools/coca-design/delete-189.png" />
-                </div>
+                    <?php } ?>
 
                 <form id="form-deletar" method="POST" action="./acoes.php">
                     <input type="hidden" name="acao" value="deletar" />
@@ -73,7 +94,7 @@ $resultado = mysqli_query($conexao, $sql);
         </section>
     </div>
     <script lang="javascript">
-        function deletar(categoriaId) {
+        function deletar(categoriaId){
             document.querySelector("#categoriaId").value = categoriaId;
             document.querySelector("#form-deletar").submit();
         }

530ba102736dec0062d2559d2b670bd86be6fdf1
cauaMascena <cauasinho656@gmail.com>
2021-10-15 11:26:21 -0300

    sla

--- produtos/acoes.php
+++ produtos/acoes.php
@@ -1,9 +1,156 @@
 <?php
 
+session_start();
+
 require("../database/conexao.php");
 
+
+
+function validarCampos(){
+
+
+    //ARRAY DAS MENSAGENS DE ERRO
+    $erros = [];
+
+    //VALIDAÇÃO DE DESCRIÇÃO
+    if ($_POST["descricao"] == "" || !isset($_POST["descricao"])) {
+        
+        $erros[] = "O CAMPO DESCRIÇÃO É OBRIGATÓRIO";
+
+    }
+    
+    //VALIDAÇÃO DE PESO
+    if ($_POST["peso"] == "" || !isset($_POST["peso"])) {
+        
+        $erros[] = "O CAMPO PESO É OBRIGATÓRIO";
+
+    }elseif (!is_numeric(str_replace(",", ".", $_POST["peso"]))) {
+        
+        $erros[] = "O CAMPO PESO DEVE SER UM NUMERO";
+
+    }
+
+    //VALIDAÇÃO DE QUANTIDADE
+    if ($_POST["quantidade"] == "" || !isset($_POST["quantidade"])) {
+        
+        $erros[] = "O CAMPO QUANTIDADE É OBRIGATÓRIO";
+
+    }elseif (!is_numeric(str_replace(",", ".", $_POST["quantidade"]))) {
+        
+        $erros[] = "O CAMPO QUANTIDADE DEVE SER UM NUMERO";
+
+    }
+
+    //VALIDAÇÃO DE COR
+    if ($_POST["cor"] == "" || !isset($_POST["cor"])) {
+        
+        $erros[] = "O CAMPO COR É OBRIGATÓRIO";
+
+    }
+
+    //VALIDAÇÃO DE COR
+    if ($_POST["tamanho"] == "" || !isset($_POST["tamanho"])) {
+        
+        $erros[] = "O CAMPO TAMANHO É OBRIGATÓRIO";
+
+    }
+
+    //VALIDAÇÃO DE VALOR
+    if ($_POST["valor"] == "" || !isset($_POST["valor"])) {
+        
+        $erros[] = "O CAMPO VALOR É OBRIGATÓRIO";
+
+    }elseif (!is_numeric(str_replace(",", ".", $_POST["quantidade"]))) {
+        
+        $erros[] = "O CAMPO VALOR DEVE SER UM NUMERO";
+
+    }
+
+    //VALIDAÇÃO DE DESCONTO
+    if ($_POST["desconto"] == "" || !isset($_POST["desconto"])) {
+        
+        $erros[] = "O CAMPO DESCONTO É OBRIGATÓRIO";
+
+    }elseif (!is_numeric(str_replace(",", ".", $_POST["desconto"]))) {
+        
+        $erros[] = "O CAMPO DESCONTO DEVE SER UM NUMERO";
+
+    }
+
+    //VALIDAÇÃO DE CATEGORIA
+    if ($_POST["categoria"] == "" || !isset($_POST["categoria"])) {
+        
+        $erros[] = "O CAMPO CATEGORIA É OBRIGATÓRIO";
+
+    }
+
+    /* VALIDAÇÃO DA IMAGEM */
+    if ($_FILES["foto"]["error"] == UPLOAD_ERR_NO_FILE) {
+        
+        $erros[] = "O ARQUIVO PRECISA SER UMA IMAGEM";
+
+    }else{
+
+        $imagemInfos = getimagesize($_FILES["foto"]["tmp"]);
+
+        if ($_FILES["foto"]["size"] > 1024 * 1024 * 2) {
+            
+            $erros[] = "O ARQUIVO NÃO PODE SER MAIOR QUE 2MB";
+
+        }
+
+        $width = $imagemInfos[0];
+        $height = $imagemInfos[1];
+
+        if ($width != $height) {
+            
+            $erros[] = "A IMAGEM PRECISA SER QUADRADA";
+
+        }
+
+    }
+
+    return $erros;
+
+}
+
+
+
 switch ($_POST["acao"]) {
+    case 'deletar':
+
+        $produtoId = $_POST['produtoId'];
+
+        $sql = "SELECT imagem FROM tbl_produto WHERE id = $produtoId";
+
+        $resultado = mysqli_query($conexao, $sql);
+
+        $produto = mysqli_fetch_array($resultado);
+
+        $sql = "DELETE FROM tbl_produto WHERE id = $produtoId";
+
+        $resultado = mysqli_query($conexao, $sql);
+
+        unlink("./fotos/" . $produto["0"]);
+
+        header('location: index.php');
+
+        break;
+
+
     case 'inserir':
+
+        $erros = validarCampos();
+
+        if (count($erros) > 0) {
+            
+            $_SESSION["erros"] = $erros;
+
+            header("location: novo/index.php");
+
+            exit;
+
+        }
 
         // TRATAMENTO DA IMAGEM PARA UPLOAD
 
--- produtos/index.php
+++ produtos/index.php
@@ -37,19 +37,19 @@ $resultado = mysqli_query($conexao, $sql);
 
             <!-- BOTÕES DE INSERÇÃO DE PRODUTOS E CATEGORIAS -->
             <!-- CASO O USUÁRIO ESTEJA LOGADO EXIBE OS BOTÕES DE CADASTRO -->
-    
-                <header>
-                    <button onclick="javascript:window.location.href ='./novo/'">Novo Produto</button>
-                    <button onclick="javascript:window.location.href ='../categorias/'">Adicionar Categoria</button>
-                </header>
+
+            <header>
+                <button onclick="javascript:window.location.href ='./novo/'">Novo Produto</button>
+                <button onclick="javascript:window.location.href ='../categorias/'">Adicionar Categoria</button>
+            </header>
 
             <main>
 
                 <!-- LISTAGEM DE PRODUTOS (INICIO) -->
 
                 <?php
-                
-                  while ($produto = mysqli_fetch_array($resultado)) {
+
+                while ($produto = mysqli_fetch_array($resultado)) {
 
                     $valor = $produto["valor"];
                     $desconto = $produto["desconto"];
@@ -57,7 +57,6 @@ $resultado = mysqli_query($conexao, $sql);
                     if ($desconto > 0) {
 
                         $valorDesconto = ($desconto / 100) * $valor;
-
                     }
 
                     $qtdParcelas = $valor > 1000 ? 12 : 6;
@@ -67,54 +66,54 @@ $resultado = mysqli_query($conexao, $sql);
                     // $valor -= $valorDesconto;
 
                     $valorParcela = $valorComDesconto / $qtdParcelas;
-                
+
                 ?>
 
-                <article class="card-produto">
+                    <article class="card-produto">
 
-                       <div class="acoes-produtos">
-                    <img onclick="javascript: window.location = './editar/?id=<?= $produto['id'] ?>'" src="../imgs/edit.svg" />
-                    <img onclick="deletar(<?= $produto['id'] ?>)" src="../imgs/trash.svg" />
-                    </div>
-    
-                <figure>
-                     <img src="fotos/<?php echo $produto["imagem"]?>" />
-                </figure>
+                        <div class="acoes-produtos">
+                            <img onclick="javascript: window.location = './editar/?id=<?= $produto['id'] ?>'" src="../imgs/edit.svg" />
+                            <img onclick="deletar(<?= $produto['id'] ?>)" src="../imgs/trash.svg" />
+                        </div>
 
-                <section>
+                        <figure>
+                            <img src="fotos/<?php echo $produto["imagem"] ?>" />
+                        </figure>
 
-                    <span class="preco">
-                        R$ <?php echo number_format($valorComDesconto, 2, ',', '.');?>
-                        <em> <?php echo $desconto; ?> % off</em>
-                    </span>
+                        <section>
 
-                    <span class="parcelamento">ou em
-                        <em>
-                        <?php echo $qtdParcelas; ?> x R$ <?php echo number_format($valorParcela, 2, ",", ".")?> sem juros
-                        </em>
-                    </span>
+                            <span class="preco">
+                                R$ <?php echo number_format($valorComDesconto, 2, ',', '.'); ?>
+                                <em> <?php echo $desconto; ?> % off</em>
+                            </span>
 
-                    <span class="descricao"><?php echo $produto["descricao"]?></span>
+                            <span class="parcelamento">ou em
+                                <em>
+                                    <?php echo $qtdParcelas; ?> x R$ <?php echo number_format($valorParcela, 2, ",", ".") ?> sem juros
+                                </em>
+                            </span>
 
-                    <span class="categoria">
-                        <em><?php echo $produto["descricao"];?></em>
-                     </span>
+                            <span class="descricao"><?php echo $produto["descricao"] ?></span>
 
-                </article>
+                            <span class="categoria">
+                                <em><?php echo $produto["descricao"]; ?></em>
+                            </span>
+
+                    </article>
 
                 <?php } ?>
 
-                </section>
+        </section>
 
-                <!-- LISTAGEM DE PRODUTOS (FIM) -->
+        <!-- LISTAGEM DE PRODUTOS (FIM) -->
 
-                <!-- FORM USADO PARA A EXCLUSÃO DE PRODUTOS -->
-                <form id="formDeletar" method="POST" action="./acoes.php">
-                    <input type="hidden" name="acao" value="deletar" />
-                    <input type="hidden" name="produtoId" id="produtoId" />
-                </form>
+        <!-- FORM USADO PARA A EXCLUSÃO DE PRODUTOS -->
+        <form id="formDeletar" method="POST" action="./acoes.php">
+            <input type="hidden" name="acao" value="deletar" />
+            <input type="hidden" name="produtoId" id="produtoId" />
+        </form>
 
-            </main>
+        </main>
 
         </section>
 
